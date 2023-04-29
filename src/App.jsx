42e907c1eff019ec39239cee3038c88958d09f93
Thiago Wender Gonsales <thiagowenderg@gmail.com>
2023-04-29 16:27:34 -0300

    :books: docs: Modificações

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ function App() {
 
   return (
     <div className="App">
-      
+      <section>
+        <div className='area1'></div>
+        <div className='area2'></div>
+        <div className='area3'></div>
+        <div className='area4'></div>
+      </section>
     </div>
   )
 }
